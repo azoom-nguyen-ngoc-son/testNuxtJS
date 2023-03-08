5e494cee22db79dcca85498d1ba5a1f4debd505e
“azoom-nguyen-ngoc-son” <nguyen.ngoc.son@azoom.com.jp>
2023-03-08 13:04:33 +0700

    :1: fix bug

--- components/baseLoading.vue
+++ components/baseLoading.vue
@@ -3,45 +3,42 @@
       <!-- <p>Loading...</p> -->
       <div class="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
     </div>
-  </template>
+</template>
   
-  <script>
-    export default {
-      data: () => ({
-        // loading: false
-      }),
-      props: {
-        loading: {
-            type: Boolean,
-            default: false
-        }
+<script>
+  export default {
+    props: {
+      loading: {
+        type: Boolean,
+        default: false
+      }
+    },
+    name: "baseLoading",
+    methods: {
+      start() {
+        this.loading = true
       },
-      name: "baseLoading",
-      methods: {
-        start() {
-          this.loading = true
-        },
-        finish() {
-          this.loading = false
-        }
+      finish() {
+        this.loading = false
       }
     }
-  </script>
+  }
+</script>
   
-  <style scoped>
-    .loading-page {
-      position: fixed;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-      background: rgba(255, 255, 255, 0.8);
-      text-align: center;
-      padding-top: 300px;
-      font-size: 30px;
-      font-family: sans-serif;
-    }
-    .lds-spinner {
+<style scoped>
+  .loading-page {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(255, 255, 255, 0.8);
+    text-align: center;
+    padding-top: 300px;
+    font-size: 30px;
+    font-family: sans-serif;
+  }
+.lds-spinner {
   color: official;
   display: inline-block;
   position: relative;
